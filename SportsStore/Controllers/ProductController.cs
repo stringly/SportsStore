d20e276a952298c1c469524daf64aedc6b00b60e
stringly <magnificentstringly@gmail.com>
2018-09-03 19:05:36 -0400

    End of Chap 9, but POST AddToCart isn't working

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -36,7 +36,7 @@ namespace SportsStore.Controllers
             PagingInfo = new PagingInfo {
                 CurrentPage = page,
                 ItemsPerPage = PageSize,
-                TotalItems = repository.Products.Count()
+                TotalItems = category == null ? repository.Products.Count() : repository.Products.Where(e => e.Category == category).Count()
             },
             CurrentCategory = category
         });
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -24,6 +24,8 @@ namespace SportsStore {
                     Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         public void Configure(IApplicationBuilder app,
@@ -31,6 +33,7 @@ namespace SportsStore {
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseMvc(routes => {
                 routes.MapRoute(
                     name: null,
